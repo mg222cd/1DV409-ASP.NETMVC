115fc4336c6a018b2056bb365ac71aed7d51de91
Marike Grinde <mg222cd@student.lnu.se>
2014-11-01 22:29:20 +0100

    påbörjat klasser

---
+++ MyValuableCalculator/MyValuableCalculator/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using MyValuableCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyValuableCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var model = new ElementaryMath();
+            return View(model);
+        }
+
+    }
+}
---
+++ MyValuableCalculator/MyValuableCalculator/Models/ElementaryMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyValuableCalculator.Models
+{
+    public class ElementaryMath
+    {
+        public int Op1 { get; set; }
+        public int Op2 { get; set; }
+        public int? Result { get; private set; }
+
+        public void Compute()
+        {
+            this.Result = this.Op1 + this.Op2;
+        }
+    }
+}
