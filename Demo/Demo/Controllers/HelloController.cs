a01b3e019005c4630773402e9f83bde543937339
Marike Grinde <mg222cd@student.lnu.se>
2014-11-04 21:14:12 +0100

    slutfört exempel från föreläsning 1

---
+++ Demo/Demo/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+        public ActionResult Greet()
+        {
+            var model = new Salutation();
+            return View(model);
+        }
+	}
+}
---
+++ Demo/Demo/Models/Salutation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Salutation
+    {
+        public string Greeting
+        {
+            get
+            {
+                return DateTime.Now.Hour < 12 ? "Good morning" : "Good afternoon";
+            }
+        }
+    }
+}
