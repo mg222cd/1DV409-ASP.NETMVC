658f142838e157647c619171da9b3acff7a998c9
Marike Grinde <mg222cd@student.lnu.se>
2014-11-04 23:34:40 +0100

    påbörjat GeekGuestbook (demo föreläsning 2)

---
+++ GeekGuestbook/GeekGuestbook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeekGuestbook.Startup))]
+namespace GeekGuestbook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
