96816e1c0426741efa9e6bc49864dc1a68578b91
Marike Grinde <mg222cd@student.lnu.se>
2014-11-05 09:13:31 +0100

    kladdat med get, create, post

---
+++ GeekGuestbook/GeekGuestbook/Controllers/GuestbookController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekGuestbook.Models;
+
+namespace GeekGuestbook.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        private GeekGuestbookEntities _enteties = new GeekGuestbookEntities();
+
+        //
+        // GET: /Guestbook/
+        public ActionResult Index()
+        {
+            var model = _enteties.Messages.ToList();
+
+            return View(model);
+        }
+
+        //
+        // POST: /Guestbook/Create
+        [HttpPost]
+        public ActionResult Create(Message model)
+        {
+            return View();
+        }
+
+
+	}
+}
