652935a8130fd69f2cce9ef28db6d3c780d4e57b
Marike Grinde <mg222cd@student.lnu.se>
2014-11-14 22:25:10 +0100

    Skrivit klassen XmlRepository

--- NextBirthday/NextBirthday/Controllers/BirthdayController.cs
+++ NextBirthday/NextBirthday/Controllers/BirthdayController.cs
@@ -14,16 +14,8 @@ namespace NextBirthday.Controllers
         // GET: /Birthday/
         public ActionResult Index()
         {
-            var path = Server.MapPath("~/App_Data/birthdates.xml");
-            var doc = XDocument.Load(path);
-            var model = (from birthdate in doc.Descendants("birthdate")
-                         select new Birthday 
-                         {
-                             Name = birthdate.Element("name").Value,
-                             Birthdate = DateTime.Parse(birthdate.Element("date").Value),
-                         }).OrderBy(b => b.DaysUntilNextBirthday).ToList();
-
-            return View(model);
+           
+            return View();
         }
 
         //
---
+++ NextBirthday/NextBirthday/Models/Repositories/XmlRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace NextBirthday.Models.Repositories
+{
+    public class XmlRepository
+    {
+        private static readonly string PhysicalPath;
+
+        private XDocument _document;
+
+        private XDocument Document
+        {
+            get
+            {
+                return _document ?? (_document = XDocument.Load(PhysicalPath));
+            }
+        }
+
+        static XmlRepository()
+        {
+            PhysicalPath = Path.Combine(
+                AppDomain.CurrentDomain.GetData("DataDirectory").ToString(),
+                "Birthdates.xml");
+        }
+
+        public IEnumerable<Birthday> GetBirthdays()
+        {
+            return (from birthdate in Document.Descendants("birthdate")
+                    select new Birthday
+                    {
+                        Name = birthdate.Element("name").Value,
+                        Birthdate = DateTime.Parse(birthdate.Element("date").Value)
+                    }).OrderBy(b => b.DaysUntilNextBirthday).ToList();
+        }
+
+        public void InsertBirthday(Birthday birthday)
+        {
+            Document.Root.Add(
+                new XElement("birthdate",
+                        new XElement("name", birthday.Name),
+                        new XElement("date", birthday.Birthdate)));
+        }
+
+        public void Save()
+        {
+            Document.Save(PhysicalPath);
+        }
+    }
+}
