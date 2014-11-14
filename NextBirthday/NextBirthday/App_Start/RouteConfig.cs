b038ee78cffad815c25a89a155ea344b626347d8
Marike Grinde <mg222cd@student.lnu.se>
2014-11-14 15:37:52 +0100

    tagit bort menyn

--- NextBirthday/NextBirthday/App_Start/RouteConfig.cs
+++ NextBirthday/NextBirthday/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NextBirthday
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Birthday", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
