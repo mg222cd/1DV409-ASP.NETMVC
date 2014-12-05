8d8bd342c16370aeb2d4a1214cc588afae1ed1ca
Marike Grinde <mg222cd@student.lnu.se>
2014-12-05 16:02:37 +0100

    påbörjat WebServices

---
+++ GeekTweet.START/GeekTweet.START/Models/Tweet.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeekTweet.START.Models
+{
+    public class Tweet
+    {
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+        public Tweet(JToken tweetToken)
+        {
+            //översätt det som tweetToken innehåller till CreatedAt, Name och Text
+            //man kan tänka sig det som en associativ array, där namnet i json är samma som nyckeln. Är nästlat i nivårer.
+            //CreatedAt = (tweetToken[""]).ToString();
+            Name = (tweetToken["user"]["name"]).ToString();
+            Text = (tweetToken["text"]).ToString();
+        }
+    }
+}
---
+++ GeekTweet.START/GeekTweet.START/Models/Webservices/TwitterWebservice.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace GeekTweet.START.Models.Webservices
+{
+    public class TwitterWebservice
+    {
+        public IEnumerable<Tweet> GetUserTimeline(string screenName)
+        {
+            string rawJson;
+
+            using(StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("~/App_Data/scottgu_timeline.json")))
+            {
+                //Läser in och stoppar in textfilen i en sträng.
+                rawJson = reader.ReadToEnd();
+            }
+
+            //gör om textfilen från Json till listobjekt med ref till tweet-objekt
+            return JArray.Parse(rawJson).Select(t => new Tweet(t)).ToList();
+        }
+    }
+}
